5404e3af532b8871dd4710e3268d9042733f0e7a
Cardon Vincent <cardon.vincent@hotmail.fr>
2025-03-13 16:33:09 +0100

    add first dag

---
+++ dags/dag.py
@@ -0,0 +1,17 @@
+from airflow.decorators import task
+from airflow.models.dag import dag
+from airflow.operators.empty import EmptyOperator
+ 
+@task
+def get_data():
+    print("Télechargement des données")
+
+@dag()
+def flights_pipeline():
+    (
+        EmptyOperator(task_id="start")
+        >> get_data()
+        >> EmptyOperator(task_id="end")
+    )
+ 
+flight_pipeline_dag = flights_pipeline()
